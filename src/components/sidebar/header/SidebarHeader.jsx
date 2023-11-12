d49ea840c5c204cdb892010ae3777899b76f7bd1
Abishek0612 <uabishek6@gmail.com>
2023-11-12 10:07:31 -0800

    Worked on logout

--- src/components/sidebar/header/SidebarHeader.jsx
+++ src/components/sidebar/header/SidebarHeader.jsx
@@ -1,9 +1,17 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import {ChatIcon, CommunityIcon, DotsIcon, StoryIcon} from "../../../svgs/svg"
+import React, { useState } from "react";
+import {  useSelector } from "react-redux";
+import {
+  ChatIcon,
+  CommunityIcon,
+  DotsIcon,
+  StoryIcon,
+} from "../../../svgs/svg";
+import Menu from "./Menu";
 
 export const SidebarHeader = () => {
   const { user } = useSelector((state) => state?.user);
+  const [showMenu, setShowMenu] = useState(false);
+
   return (
     <div className="h-[50px] dark:bg-dark_bg_2 flex items-center p16">
       {/* Container */}
@@ -18,27 +26,28 @@ export const SidebarHeader = () => {
         </button>
         {/* user icons */}
         <ul className="flex items-center gap-x-2 5">
-            <li>
-                <button className="btn">
-                    <CommunityIcon className="dark:fill-dark_svg_1"  />
-                </button>
-            </li>
-            <li>
-                <button className="btn">
-                    <StoryIcon className="dark:fill-dark_svg_1"  />
-                </button>
-            </li>
-            <li>
-                <button className="btn">
-                    <ChatIcon className="dark:fill-dark_svg_1"  />
-                </button>
-            </li>
+          <li>
+            <button className="btn">
+              <CommunityIcon className="dark:fill-dark_svg_1" />
+            </button>
+          </li>
+          <li>
+            <button className="btn">
+              <StoryIcon className="dark:fill-dark_svg_1" />
+            </button>
+          </li>
+          <li>
+            <button className="btn">
+              <ChatIcon className="dark:fill-dark_svg_1" />
+            </button>
+          </li>
 
-            <li>
-                <button className="btn">
-                    <DotsIcon className="dark:fill-dark_svg_1"  />
-                </button>
-            </li>
+          <li className="relative" onClick={() => setShowMenu((prev) => !prev)}>
+            <button className={`btn ${showMenu} ? "bg-dark_hover_1" : ""`}>
+              <DotsIcon className="dark:fill-dark_svg_1" />
+            </button>
+            {showMenu ? <Menu /> : null}
+          </li>
         </ul>
       </div>
     </div>
